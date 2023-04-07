93170d437c4c397f25c3edbdc79deb1ef54dacbc
Johnny <joaogbdesouza@gmail.com>
2023-04-07 14:23:44 -0300

    testing persistence in a json file

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
-mod to_do;
+mod state;
 
-use to_do::enum_factory::to_do_factory;
-use to_do::enums::TaskStatus;
-use to_do::enum_factory::ItemTypes;
-// use to_do::traits::create::Create;
-use to_do::traits::edit::Edit;
-use to_do::traits::delete::Delete;
-use to_do::traits::get::Get;
+use std::env;
+use state::{write_to_file, read_file};
+use serde_json::value::Value;
+use serde_json::{Map, json};
 
 fn main() {
-    let to_do_item = to_do_factory("washing", TaskStatus::DONE);
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
 
-    match to_do_item {
-        ItemTypes::Done(item) => {
-            item.get(&item.super_struct.title);
-            item.delete(&item.super_struct.title);
-        },
-        ItemTypes::Pending(item) => {
-            item.get(&item.super_struct.title);
-            item.set_to_done(&item.super_struct.title);
-        }
-    }
+    let mut state: Map<String, Value> = read_file("./state.json");
+    println!("Before operation: {:?}", state);
 
+    state.insert(title.to_string(), json!(status));
+    println!("After operation: {:?}", state);
+
+    write_to_file("./state.json", &mut state);
 }
