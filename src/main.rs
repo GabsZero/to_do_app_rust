00606355c16feb9a22711b83299228f643bb9158
Johnny <joaogbdesouza@gmail.com>
2023-04-07 13:52:32 -0300

    using traits in main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@ mod to_do;
 
 use to_do::enum_factory::to_do_factory;
 use to_do::enums::TaskStatus;
-
 use to_do::enum_factory::ItemTypes;
+// use to_do::traits::create::Create;
+use to_do::traits::edit::Edit;
+use to_do::traits::delete::Delete;
+use to_do::traits::get::Get;
 
 fn main() {
-    let to_do_item = to_do_factory("washing", TaskStatus::PENDING);
+    let to_do_item = to_do_factory("washing", TaskStatus::DONE);
+
     match to_do_item {
         ItemTypes::Done(item) => {
-            println!("Title: {}", item.super_struct.title);
-            println!("Status: {}", item.super_struct.status);
+            item.get(&item.super_struct.title);
+            item.delete(&item.super_struct.title);
         },
         ItemTypes::Pending(item) => {
-            println!("Title: {}", item.super_struct.title);
-            println!("Status: {}", item.super_struct.status);
+            item.get(&item.super_struct.title);
+            item.set_to_done(&item.super_struct.title);
         }
     }
 
