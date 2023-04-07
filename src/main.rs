8402bc1b029db665231e151067131944a9eb21ad
Johnny <joaogbdesouza@gmail.com>
2023-04-07 20:49:24 -0300

    controlling to do list from main and persisting in json file

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 mod state;
+mod to_do;
+mod processes;
 
 use std::env;
 use state::{write_to_file, read_file};
 use serde_json::value::Value;
 use serde_json::{Map, json};
+use to_do::enum_factory::to_do_factory;
+use to_do::enums::TaskStatus;
+use processes::process_input;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let status: &String = &args[1];
+    let command: &String = &args[1];
     let title: &String = &args[2];
 
     let mut state: Map<String, Value> = read_file("./state.json");
-    println!("Before operation: {:?}", state);
+    let status: String;
 
-    state.insert(title.to_string(), json!(status));
-    println!("After operation: {:?}", state);
+    match &state.get(*&title) {
+        Some(result) => {
+            status = result.to_string().replace('\"', "");
+        },
+        None => {
+            status = "pending".to_owned();
+        }
+    }
 
-    write_to_file("./state.json", &mut state);
+    let item = to_do_factory(title, TaskStatus::from_string(status.to_uppercase()));
+    process_input(item, command.to_string(), &state);
 }
