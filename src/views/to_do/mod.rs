2e368942f327ba5d68305e63c45ee2ca8941c8e1
Johnny <joaogbdesouza@gmail.com>
2023-04-08 15:41:00 -0300

    changing create endpoint to post

--- src/views/to_do/mod.rs
+++ src/views/to_do/mod.rs
@@ -1,9 +1,9 @@
 mod create;
 
-use actix_web::web::{ServiceConfig, get, scope};
+use actix_web::web::{ServiceConfig, get, post, scope};
 pub fn to_do_views_factory(app: &mut ServiceConfig) {
     app.service(
         scope("v1/item")
-        .route("create/{title}", get().to(create::create))
+        .route("create/{title}", post().to(create::create))
     );
 }
